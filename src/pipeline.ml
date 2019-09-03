15fb2a37bcaa76a63e973ab261b5b99f445dd0ce
Thomas Leonard <talex5@gmail.com>
2019-09-03 18:52:49 +0100

    Pass all directories containing opam files to opam install
    Otherwise, it tries to install the (empty) pinned versions of packages
    in subdirectories.

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -14,18 +14,23 @@ let weekly = Current_cache.Schedule.v ~valid_for:(Duration.of_day 7) ()
 
 let crunch_list items = Dockerfile.(crunch (empty @@@ items))
 
+(* Group opam files by directory.
+   e.g. ["a/a1.opam"; "a/a2.opam"; "b/b1.opam"] ->
+        [("a", ["a/a1.opam"; "a/a2.opam"]);
+         ("b", ["b/b1.opam"])
+        ] *)
+let group_opam_files =
+  ListLabels.fold_left ~init:[] ~f:(fun acc x ->
+      let item = Fpath.v x in
+      let dir = Fpath.parent item in
+      match acc with
+      | (prev_dir, prev_items) :: rest when Fpath.equal dir prev_dir -> (prev_dir, x :: prev_items) :: rest
+      | _ -> (dir, [x]) :: acc
+    )
+
 (* Generate Dockerfile instructions to copy all the files in [items] into the
    image, creating the neccesary directories first, and then pin them all. *)
-let pin_opam_files items =
-  let groups =
-    ListLabels.fold_left ~init:[] items ~f:(fun acc x ->
-        let item = Fpath.v x in
-        let dir = Fpath.parent item in
-        match acc with
-        | (prev_dir, prev_items) :: rest when Fpath.equal dir prev_dir -> (prev_dir, x :: prev_items) :: rest
-        | _ -> (dir, [x]) :: acc
-      )
-  in
+let pin_opam_files groups =
   let open Dockerfile in
   let dirs = groups |> List.map (fun (dir, _) -> Printf.sprintf "%S" (Fpath.to_string dir)) |> String.concat " " in
   (run "mkdir -p %s" dirs @@@ (
@@ -44,12 +49,14 @@ let pin_opam_files items =
 
 (* Generate a Dockerfile for building all the opam packages in the build context. *)
 let dockerfile ~base ~opam_files =
+  let groups = group_opam_files opam_files in
+  let dirs = groups |> List.map (fun (dir, _) -> Printf.sprintf "%S" (Fpath.to_string dir)) |> String.concat " " in
   let open Dockerfile in
   from (Docker.Image.hash base) @@
   workdir "/src" @@
   run "sudo chown opam /src" @@
-  pin_opam_files opam_files @@
-  run "opam install . --show-actions --deps-only -t | awk '/- install/{print $3}' | xargs opam depext -iy" @@
+  pin_opam_files groups @@
+  run "opam install %s --show-actions --deps-only -t | awk '/- install/{print $3}' | xargs opam depext -iy" dirs @@
   copy ~chown:"opam" ~src:["."] ~dst:"/src/" () @@
   run "opam install -tv ."
 
