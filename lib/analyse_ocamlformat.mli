d5043c1187c1c317eb8344e7725efab21672e395
Jules Aguillon <juloo.dsi@gmail.com>
2019-12-18 15:02:33 +0100

    Update lib/analyse_ocamlformat.mli
    Co-Authored-By: Craig Ferguson <me@craigfe.io>

--- lib/analyse_ocamlformat.mli
+++ lib/analyse_ocamlformat.mli
@@ -1,6 +1,6 @@
 type source =
   | Opam of { version : string } (** Should install OCamlformat from Opam. *)
-  | Vendored of { path : string } (** OCamlformat is vendored. [path] is relative from the project's root. *)
+  | Vendored of { path : string } (** OCamlformat is vendored. [path] is relative to the project's root. *)
 [@@deriving yojson,eq]
 
 val get_ocamlformat_source : Current.Job.t -> opam_files:string list -> Fpath.t -> source option Lwt.t
