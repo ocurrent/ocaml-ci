af7163bbce84cf06abe09dc5151cb07877d3a3fd
Jules Aguillon <juloo.dsi@gmail.com>
2019-12-20 13:23:48 +0100

    Update lib/analyse_ocamlformat.mli
    Co-Authored-By: Craig Ferguson <me@craigfe.io>

--- lib/analyse_ocamlformat.mli
+++ lib/analyse_ocamlformat.mli
@@ -4,5 +4,5 @@ type source =
 [@@deriving yojson,eq]
 
 val get_ocamlformat_source : Current.Job.t -> opam_files:string list -> Fpath.t -> source option Lwt.t
-(** Detect the required version of ocamlformat or if it's vendored.
+(** Detect the required version of OCamlformat or if it's vendored.
     Vendored OCamlformat is detected by looking at file names in [opam_files]. *)
