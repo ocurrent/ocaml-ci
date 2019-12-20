4227bc664d6374b4d5dd842a7c078696898e3145
Jules Aguillon <juloo.dsi@gmail.com>
2019-12-20 16:39:36 +0100

    Copy files before pinning vendored OCamlformat

--- service/lint.ml
+++ service/lint.ml
@@ -3,21 +3,25 @@ module Docker = Conf.Builder_amd1
 
 let ocamlformat_dockerfile ~base ~ocamlformat_source =
   let open Dockerfile in
-  let install_ocamlformat =
+  let copy_files =
+    copy ~chown:"opam" ~src:["./"] ~dst:"./" ()
+  in
+  let install_ocamlformat_and_copy_files =
     match ocamlformat_source with
     | Ocaml_ci.Analyse_ocamlformat.Vendored { path } ->
-      run "opam pin add -yn ocamlformat %S" path
+      copy_files
+      @@ run "opam pin add -yn ocamlformat %S" path
       @@ run "opam depext ocamlformat"
       @@ run "opam install --deps-only -y ocamlformat"
     | Opam { version } ->
       run "opam depext ocamlformat=%s" version
       @@ run "opam install ocamlformat=%s" version
+      @@ copy_files
   in
   from (Docker.Image.hash base)
   @@ run "opam install dune" (* Not necessarily the dune version used by the project *)
   @@ workdir "src"
-  @@ install_ocamlformat
-  @@ copy ~chown:"opam" ~src:["./"] ~dst:"./" ()
+  @@ install_ocamlformat_and_copy_files
 
 let v_fmt ~ocamlformat_source ~base ~src =
   let dockerfile =
