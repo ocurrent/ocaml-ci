ede64be24a066660776df468e301f2266ef7724e
Jules Aguillon <juloo.dsi@gmail.com>
2019-12-20 16:54:51 +0100

    Only copy the opam file to be more cache efficient

--- service/lint.ml
+++ service/lint.ml
@@ -3,25 +3,25 @@ module Docker = Conf.Builder_amd1
 
 let ocamlformat_dockerfile ~base ~ocamlformat_source =
   let open Dockerfile in
-  let copy_files =
-    copy ~chown:"opam" ~src:["./"] ~dst:"./" ()
-  in
-  let install_ocamlformat_and_copy_files =
+  let install_ocamlformat =
     match ocamlformat_source with
     | Ocaml_ci.Analyse_ocamlformat.Vendored { path } ->
-      copy_files
-      @@ run "opam pin add -yn ocamlformat %S" path
+      let opam_file = Filename.concat path "ocamlformat.opam" in
+      copy ~chown:"opam" ~src:[ opam_file ] ~dst:opam_file ()
+      @@ run "opam pin add -k none -yn ocamlformat %S" path
+      (* Only the opam file is necessary to install the deps
+         [-k none] above ensures that opam doesn't try to make an installable package *)
       @@ run "opam depext ocamlformat"
       @@ run "opam install --deps-only -y ocamlformat"
     | Opam { version } ->
       run "opam depext ocamlformat=%s" version
       @@ run "opam install ocamlformat=%s" version
-      @@ copy_files
   in
   from (Docker.Image.hash base)
   @@ run "opam install dune" (* Not necessarily the dune version used by the project *)
   @@ workdir "src"
-  @@ install_ocamlformat_and_copy_files
+  @@ install_ocamlformat
+  @@ copy ~chown:"opam" ~src:["./"] ~dst:"./" ()
 
 let v_fmt ~ocamlformat_source ~base ~src =
   let dockerfile =
