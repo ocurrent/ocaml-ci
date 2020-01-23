fe5fdc04ef1f71c82fa1ee9be2ae7e6eab44416c
Jules Aguillon <juloo.dsi@gmail.com>
2020-01-23 14:42:04 +0100

    Improve Lint.v doc
    Co-Authored-By: Craig Ferguson <me@craigfe.io>

--- lib/lint.mli
+++ lib/lint.mli
@@ -5,8 +5,8 @@ module Make (Docker : S.DOCKER_CONTEXT) : sig
     source:Docker.source ->
     [> `Checked | `Check_skipped ] Current.t
   (** [v ~analysis ~src] runs the linting step:
+      - Ensures OCamlformat is installed depending on {!Analysis.ocamlformat_source}.
       - Checks formatting using "dune build @fmt".
-        Ensures OCamlformat is installed depending on [Analysis.ocamlformat_source].
       The base image [base] should have OPAM installed. *)
 
 end
