a8bd86e38aa04f4af18ae98e3f22860e42b77a1f
Kate <kit.ty.kate@disroot.org>
2020-02-05 19:34:51 +0000

    tmp

--- service/opam_repo_pipeline.mli
+++
@@ -1,5 +0,0 @@
-val local_test : Current_git.Local.t -> unit -> unit Current.t
-(** [local_test repo] is a pipeline that tests local repository [repo] as the CI would. *)
-
-val v : app:Current_github.App.t -> unit -> unit Current.t
-(** The main ocaml-ci pipeline. Tests everything configured for GitHub application [app]. *)
