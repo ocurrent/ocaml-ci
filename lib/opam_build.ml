36c0260876b24f93b54ba100bd668a3cae149813
Kate <kit.ty.kate@disroot.org>
2020-03-05 15:32:46 +0000

    Fix setups that don't have recent enough git commands

--- lib/opam_build.ml
+++ lib/opam_build.ml
@@ -16,10 +16,11 @@ let dockerfile { base; pkg; variant} =
   in
   comment "syntax = docker/dockerfile:experimental@sha256:ee85655c57140bd20a5ebc3bb802e7410ee9ac47ca92b193ed0ab17485024fe5" @@
   from base @@
+  workdir "/home/opam/opam-repository" @@
   comment "%s" variant @@
   distro_extras @@
   copy ~chown:"opam" ~src:["."] ~dst:"/src/" () @@
-  run "git -C /home/opam/opam-repository pull origin master && git -C /home/opam/opam-repository pull /src && opam update default" @@
+  run "git pull origin master && git pull /src && opam update default" @@
   run "%s opam depext -ivy %s" download_cache pkg
 
 let cache = Hashtbl.create 10000
