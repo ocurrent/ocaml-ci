8df9150c145ef0b6ccd63f046b88e9d9f947570b
Kate <kit.ty.kate@disroot.org>
2020-04-10 19:21:55 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ex
+
+docker build -t ocaml-ci-web -f Dockerfile.web .
+docker build -t ocaml-ci-service -f Dockerfile .
+docker stack rm opam-ci
+sleep 10
+docker stack deploy -c stack.yml opam-ci
