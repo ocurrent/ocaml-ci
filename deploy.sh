6ec6bb3e9e97e11ae6ab189e2a55f6d630d8d9eb
Kate <kit.ty.kate@disroot.org>
2020-04-11 14:38:49 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 docker build -t ocaml-ci-web -f Dockerfile.web .
 docker build -t ocaml-ci-service -f Dockerfile .
 docker stack rm opam-ci
-sleep 10
+sleep 15
 docker stack deploy -c stack.yml opam-ci
