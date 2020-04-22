43b18ad793257947194834847e9afcb5a78a5c7f
Thomas Leonard <talex5@gmail.com>
2020-04-22 16:56:16 +0100

    Perform rebuilds in the background if the commit didn't change
    Before, we would turn the GitHub status orange if any part of the key
    (commit, base image hash, analysis) changed. Now, we only consider the
    commit to be the key. Other changes rebuild but latch the previous build
    result until the operation is complete.

--- lib/build.ml
+++ lib/build.ml
@@ -12,20 +12,12 @@ module Op = struct
     type t = {
       commit : Current_git.Commit.t;            (* The source code to build and test *)
       repo : Current_github.Repo_id.t;          (* Used to choose a build cache *)
-      base : Raw.Image.t;                       (* The image with the OCaml compiler to use. *)
       variant : string;                         (* Added as a comment in the Dockerfile *)
-      analysis : Analyse.Analysis.t;
     }
 
-    let digest_analysis x =
-      let s = Analyse.Analysis.to_yojson x |> Yojson.Safe.to_string in
-      `String (Digest.string s |> Digest.to_hex)
-
-    let to_json { commit; analysis; base; variant; repo } =
+    let to_json { commit; variant; repo } =
       `Assoc [
         "commit", `String (Current_git.Commit.hash commit);
-        "analysis", digest_analysis analysis;
-        "base", `String (Raw.Image.digest base);
         "variant", `String variant;
         "repo", `String (Fmt.to_to_string Current_github.Repo_id.pp repo);
       ]
@@ -33,13 +25,33 @@ module Op = struct
     let digest t = Yojson.Safe.to_string (to_json t)
   end
 
-  module Value = Current.Unit
+  module Value = struct
+    type t = {
+      base : Raw.Image.t;                       (* The image with the OCaml compiler to use. *)
+      analysis : Analyse.Analysis.t;
+    }
+
+    let digest_analysis x =
+      let s = Analyse.Analysis.to_yojson x |> Yojson.Safe.to_string in
+      `String (Digest.string s |> Digest.to_hex)
+
+    let to_json { analysis; base } =
+      `Assoc [
+        "analysis", digest_analysis analysis;
+        "base", `String (Raw.Image.digest base);
+      ]
+
+    let digest t = Yojson.Safe.to_string (to_json t)
+  end
+
+  module Outcome = Current.Unit
 
   let or_raise = function
     | Ok () -> ()
     | Error (`Msg m) -> raise (Failure m)
 
-  let build { Builder.docker_context; pool; build_timeout } job { Key.commit; base; analysis; variant; repo } =
+  let run { Builder.docker_context; pool; build_timeout } job
+      { Key.commit; variant; repo } { Value.base; analysis } =
     let make_dockerfile =
       let base = Raw.Image.hash base in
       if Analyse.Analysis.is_duniverse analysis then
@@ -66,16 +78,17 @@ module Op = struct
     let pp_error_command f = Fmt.string f "Docker build" in
     Current.Process.exec ~cancellable:true ~pp_error_command ~job cmd
 
-  let pp f { Key.repo; commit; variant; _ } =
+  let pp f ({ Key.repo; commit; variant }, _) =
     Fmt.pf f "@[<v2>test %a %a on %s@]"
       Current_github.Repo_id.pp repo
       Current_git.Commit.pp commit
       variant
 
   let auto_cancel = true
+  let latched = true
 end
 
-module BC = Current_cache.Make(Op)
+module BC = Current_cache.Generic(Op)
 
 let pull ~schedule platform =
   Current.component "docker pull" |>
@@ -89,7 +102,7 @@ let build ~repo ~analysis ~platform ~base commit =
   and> base = base
   and> commit = commit
   and> repo = repo in
-  BC.get builder { Op.Key.commit; analysis; repo; base; variant }
+  BC.run builder { Op.Key.commit; repo; variant } { Op.Value.base; analysis}
 
 let v ~platform ~schedule ~repo ~analysis source =
   let base = pull ~schedule platform in
