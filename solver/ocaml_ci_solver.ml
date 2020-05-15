0fbc5db7ebb88dbad92ce583db32c5a6ec861b35
Thomas Leonard <talex5@gmail.com>
2020-05-15 15:36:58 +0100

    Include timestamps in solver log output

--- solver/ocaml_ci_solver.ml
+++ solver/ocaml_ci_solver.ml
@@ -1,8 +1,26 @@
 module Worker = Ocaml_ci_api.Worker
 
+let pp_timestamp f x =
+  let open Unix in
+  let tm = gmtime x in
+  Fmt.pf f "%04d-%02d-%02d %02d:%02d.%02d" (tm.tm_year + 1900) (tm.tm_mon + 1)
+    tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec
+
+let reporter =
+  let report src level ~over k msgf =
+    let k _ = over (); k () in
+    let src = Logs.Src.name src in
+    msgf @@ fun ?header ?tags:_ fmt ->
+    Fmt.kpf k Fmt.stderr ("%a %14s %a @[" ^^ fmt ^^ "@]@.")
+      pp_timestamp (Unix.gettimeofday ())
+      src
+      Logs_fmt.pp_header (level, header)
+  in
+  { Logs.report = report }
+
 let main () =
   Logs.(set_level (Some Info));
-  Logs.set_reporter @@ Logs_fmt.reporter ();
+  Logs.set_reporter reporter;
   match Worker.Solve_request.of_yojson (Yojson.Safe.from_channel stdin) with
   | Error msg -> Fmt.failwith "Bad request: %s" msg
   | Ok request ->
