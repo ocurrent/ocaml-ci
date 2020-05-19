553ccf0b090d0de53337592fafd59799e404f767
Thomas Leonard <talex5@gmail.com>
2020-05-19 13:52:30 +0100

    Use a pool of long-running solver processes
    This is to make it easier to run multiple solver services in future.

--- solver/ocaml_ci_solver.ml
+++ solver/ocaml_ci_solver.ml
@@ -1,20 +1,100 @@
-module Worker = Ocaml_ci_api.Worker
+open Lwt.Infix
 
-let run_toplevel () =
+module Worker = Ocaml_ci_api.Worker
+module Selection = Worker.Selection
+
+(* Send [request] to [worker] and read the reply. *)
+let process ~id request worker =
+  let request_str = Worker.Solve_request.to_yojson request |> Yojson.Safe.to_string in
+  let request_str = Printf.sprintf "%d\n%s" (String.length request_str) request_str in
+  Lwt_io.write worker#stdin request_str >>= fun () ->
+  Lwt_io.read_line worker#stdout >>= fun time ->
+  Lwt_io.read_line worker#stdout >>= fun len ->
+  let len = int_of_string len in
+  Lwt_io.read ~count:len worker#stdout >|= fun results ->
+  if String.length results = 0 then Fmt.failwith "No output from solve worker!";
+  match results.[0] with
+  | '+' ->
+    Log.info (fun f -> f "%s: found solution in %s s" id time);
+    let packages =
+      Astring.String.with_range ~first:1 results
+      |> Astring.String.cuts ~sep:" "
+    in
+    Ok packages
+  | '-' ->
+    Log.info (fun f -> f "%s: eliminated all possibilities in %s s" id time);
+    let msg = results |> Astring.String.with_range ~first:1 in
+    Error msg
+  | _ ->
+    Fmt.failwith "BUG: bad output: %s" results
+
+(* Handle a request by distributing it among the worker processes and then aggregating their responses. *)
+let handle ~pool request =
+  let { Worker.Solve_request.opam_repository; platforms; root_pkgs; pinned_pkgs } = request in
+  let opam_repository = Opam_repository.of_dir opam_repository in
+  let root_pkgs = List.map fst root_pkgs in
+  let pinned_pkgs = List.map fst pinned_pkgs in
+  let pins =
+    root_pkgs @ pinned_pkgs
+    |> List.map OpamPackage.of_string
+    |> List.map OpamPackage.name
+    |> OpamPackage.Name.Set.of_list
+  in
+  Log.info (fun f -> f "Solving for %a" Fmt.(list ~sep:comma string) root_pkgs);
+  platforms |> Lwt_list.map_p (fun p ->
+      let id = fst p in
+      let slice = { request with platforms = [p] } in
+      Lwt_pool.use pool (process ~id slice) >>= function
+      | Error _ as e -> Lwt.return (id, e)
+      | Ok packages ->
+        let repo_packages =
+          packages |> List.filter_map (fun pkg ->
+              let pkg = OpamPackage.of_string pkg in
+              if OpamPackage.Name.Set.mem pkg.name pins then None
+              else Some pkg
+            )
+        in
+        Opam_repository.oldest_commit_with opam_repository repo_packages >|= fun commit ->
+        id, Ok { Worker.Selection.id; packages; commit }
+    )
+  >|= List.filter_map (fun (id, result) ->
+      Log.info (fun f -> f "= %s =" id);
+      match result with
+      | Ok result ->
+        Log.info (fun f -> f "-> @[<hov>%a@]" Fmt.(list ~sep:sp string) result.Selection.packages);
+        Log.info (fun f -> f "(valid since opam-repository commit %s)" result.Selection.commit);
+        Some result
+      | Error msg ->
+        Log.info (fun f -> f "%s" msg);
+        None
+    )
+
+(* Read a request from stdin, process it, and write the response on stdout. *)
+let run_toplevel pool =
   match Worker.Solve_request.of_yojson (Yojson.Safe.from_channel stdin) with
   | Error msg -> Fmt.failwith "Bad request: %s" msg
   | Ok request ->
-    let response =
-      try Ok (Solver.solve request)
-      with
-      | Failure msg -> Error (`Msg msg)
-      | ex -> Fmt.error_msg "%a" Fmt.exn ex
-    in
+    Lwt.catch
+      (fun () -> handle ~pool request >|= Result.ok)
+      (function
+        | Failure msg -> Lwt_result.fail (`Msg msg)
+        | ex -> Lwt.return (Fmt.error_msg "%a" Fmt.exn ex)
+      )
+    >|= fun response ->
     Yojson.Safe.to_channel stdout (Worker.Solve_response.to_yojson response)
 
 let main ~self args =
   Logs.(set_level (Some Info));
   Logs.set_reporter Log.reporter;
   match args with
-  | [] -> run_toplevel ()
-  | _ -> Fmt.failwith "Usage: %a" Fmt.(list string) self
+  | [] ->
+    let n_workers = 20 in
+    Lwt_main.run begin
+      let pool = Lwt_pool.create n_workers (fun () ->
+          let cmd = ("", Array.of_list (self @ ["--worker"])) in
+          Lwt.return (Lwt_process.open_process cmd)
+        ) in
+      run_toplevel pool
+    end
+  | ["--worker"] -> Solver.main ()
+  | args -> Fmt.failwith "Usage: %a (got %a)" Fmt.(list string) self Fmt.(list (quote string)) args
--- solver/solver.ml
+++ solver/solver.ml
@@ -1,8 +1,5 @@
-open Lwt.Infix
-
 module Worker = Ocaml_ci_api.Worker
 module Solver = Opam_0install.Solver.Make(Opam_0install.Dir_context)
-module Selection = Worker.Selection
 
 let env (vars : Worker.Vars.t) =
   Opam_0install.Dir_context.std_env
@@ -19,7 +16,7 @@ let parse_opam (name, contents) =
   let opam = OpamFile.OPAM.read_from_string contents in
   OpamPackage.name pkg, (OpamPackage.version pkg, opam)
 
-let run_child ~opam_repository ~pins ~root_pkgs (vars : Worker.Vars.t) =
+let solve ~opam_repository ~pins ~root_pkgs (vars : Worker.Vars.t) =
   let ocaml_version = OpamPackage.Version.of_string vars.ocaml_version in
   let context =
     Opam_0install.Dir_context.create (Opam_repository.packages_dir opam_repository)
@@ -35,114 +32,32 @@ let run_child ~opam_repository ~pins ~root_pkgs (vars : Worker.Vars.t) =
   match r with
   | Ok sels ->
     let pkgs = Solver.packages_of_result sels in
-    let packages = List.map OpamPackage.to_string pkgs in
-    Printf.printf "+%s%!" @@ String.concat " " packages
+    Ok (List.map OpamPackage.to_string pkgs)
   | Error diagnostics ->
-    let msg = Solver.diagnostics diagnostics in
-    Printf.printf "-%s%!" msg
+    Error (Solver.diagnostics diagnostics)
 
-let rec waitpid_non_intr pid =
-  try Unix.waitpid [] pid
-  with Unix.Unix_error (Unix.EINTR, _, _) -> waitpid_non_intr pid
-
-let check_exit_status = function
-  | Unix.WEXITED 0 -> ()
-  | Unix.WEXITED code -> Fmt.failwith "Child returned error exit status %d" code
-  | Unix.WSIGNALED signal -> Fmt.failwith "Child aborted (signal %d)" signal
-  | Unix.WSTOPPED signal -> Fmt.failwith "Child is currently stopped (signal %d)" signal
-
-let spawn_child run (id, vars) =
-  let r, w = Unix.pipe ~cloexec:true () in
-  match Unix.fork () with
-  | 0 -> (* We are the child *)
-    begin
-      try
-        Unix.close r;
-        Unix.dup2 w Unix.stdout;
-        run vars;
-        exit 0
-      with ex ->
-        Printf.printf "-%s\n%!" (Printexc.to_string ex);
-        exit 1
-    end
-  | child ->
-    Unix.close w;
-    id, child, r
-
-let pp_name = Fmt.of_to_string OpamPackage.Name.to_string
-
-let solve { Worker.Solve_request.opam_repository; root_pkgs; pinned_pkgs; platforms } =
-  let opam_repository = Opam_repository.of_dir opam_repository in
-  let root_pkgs = List.map parse_opam root_pkgs in
-  let pinned_pkgs = List.map parse_opam pinned_pkgs in
-  let pins =
-    root_pkgs @ pinned_pkgs
-    |> OpamPackage.Name.Map.of_list
-  in
-  let root_pkgs = List.map fst root_pkgs in
-  Log.info (fun f -> f "Solving for %a" (Fmt.(list ~sep:comma) pp_name) root_pkgs);
-  let jobs = List.map (spawn_child (run_child ~opam_repository ~pins ~root_pkgs)) platforms in
-  let results =
-    jobs |> List.map (fun (id, pid, from_child) ->
-        let buf = Bytes.create 4096 in
-        let results = Buffer.create 4096 in
-        let rec read () =
-          let got = Unix.read from_child buf 0 (Bytes.length buf) in
-          if got > 0 then (
-            Buffer.add_subbytes results buf 0 got;
-            read ()
-          ) else (
-            Unix.close from_child;
-            Buffer.contents results
-          )
+let rec main () =
+  match input_line stdin with
+  | exception End_of_file -> ()
+  | len ->
+    let len = int_of_string len in
+    let data = really_input_string stdin len in
+    let request = Worker.Solve_request.of_yojson (Yojson.Safe.from_string data) |> Result.get_ok in
+    let { Worker.Solve_request.opam_repository; root_pkgs; pinned_pkgs; platforms } = request in
+    let opam_repository = Opam_repository.of_dir opam_repository in
+    let root_pkgs = List.map parse_opam root_pkgs in
+    let pinned_pkgs = List.map parse_opam pinned_pkgs in
+    let pins =
+      root_pkgs @ pinned_pkgs
+      |> OpamPackage.Name.Map.of_list
+    in
+    let root_pkgs = List.map fst root_pkgs in
+    platforms |> List.iter (fun (_id, platform) ->
+        let msg =
+          match solve ~opam_repository ~pins ~root_pkgs platform with
+          | Ok packages -> "+" ^ String.concat " " packages
+          | Error msg -> "-" ^ msg
         in
-        let results = read () in
-        waitpid_non_intr pid |> snd |> check_exit_status;
-        match Astring.String.cut ~sep:"\n" results with
-        | None -> Fmt.failwith "Missing newline in solver results: %S" results
-        | Some (time, results) ->
-          if String.length results = 0 then Fmt.failwith "No output from solve worker!";
-          match results.[0] with
-          | '+' ->
-            Log.info (fun f -> f "%s: found solution in %s s" id time);
-            let packages =
-              Astring.String.with_range ~first:1 results
-              |> Astring.String.cuts ~sep:" "
-            in
-            (id, Ok packages)
-          | '-' ->
-            Log.info (fun f -> f "%s: eliminated all possibilities in %s s" id time);
-            let msg = results |> Astring.String.with_range ~first:1 in
-            (id, Error msg)
-          | _ ->
-            Fmt.failwith "BUG: bad output: %s" results
-      ) in
-  (* Now all the sub-processes are gone, we can safely start Lwt. *)
-  Lwt_main.run begin
-    results
-    |> Lwt_list.map_p (fun (id, output) ->
-        match output with
-        | Ok packages ->
-          let repo_packages =
-            packages |> List.filter_map (fun pkg ->
-                let pkg = OpamPackage.of_string pkg in
-                if OpamPackage.Name.Map.mem pkg.name pins then None
-                else Some pkg
-              )
-          in
-          Opam_repository.oldest_commit_with opam_repository repo_packages >|= fun commit ->
-          id, Ok { Selection.id; packages; commit }
-        | Error _ as e -> Lwt.return (id, e)
-      )
-    >|= List.filter_map (fun (id, result) ->
-        Log.info (fun f -> f "= %s =" id);
-        match result with
-        | Ok result ->
-          Log.info (fun f -> f "-> @[<hov>%a@]" Fmt.(list ~sep:sp string) result.Selection.packages);
-          Log.info (fun f -> f "(valid since opam-repository commit %s)" result.Selection.commit);
-          Some result
-        | Error msg ->
-          Log.info (fun f -> f "%s" msg);
-          None
-      )
-  end
+        Printf.printf "%d\n%s%!" (String.length msg) msg;
+      );
+    main ()
