52e83abccd2699ec3aaaab419cd4fc25e6905abd
Etienne Millon <me@emillon.org>
2020-12-07 18:13:09 +0100

    Use release

--- lib/opam_monorepo.ml
+++ lib/opam_monorepo.ml
@@ -91,14 +91,11 @@ let selections ~platforms ~info:(package, lock_file) =
 let install_opam_monorepo ~network ~cache ~monorepo_version =
   match monorepo_version with
   | "0.1" ->
-      let ref = "0.1.0" in
+      let version = "0.1.0" in
       let open Obuilder_spec in
       [
-        run ~network ~cache
-          "opam pin add -n https://github.com/ocamllabs/opam-monorepo.git#%s"
-          ref;
-        run ~network ~cache "opam depext --update -y opam-monorepo";
-        run ~network ~cache "opam install opam-monorepo";
+        run ~network ~cache "opam depext --update -y opam-monorepo.%s" version;
+        run ~network ~cache "opam install opam-monorepo.%s" version;
       ]
   | v -> Printf.ksprintf failwith "unknown opam-monorepo version %S" v
 
