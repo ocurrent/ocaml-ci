c2a425904e6d170ca8c8067357870fefcfbd4f9e
Lucas Pluvinage <lucas@tarides.com>
2021-09-21 16:28:22 +0200

    matrix: read-write room and setup a list of administrators

--- service/conf.ml
+++ service/conf.ml
@@ -8,6 +8,11 @@ let profile =
    Don't bother testing these. *)
 let max_staleness = Duration.of_day 93
 
+(* List of admins for rooms managed by the CI bot. *)
+let matrix_admins = [
+  "@avsm:recoil.org"
+]
+
 module Capnp = struct
   (* Cap'n Proto RPC is enabled by passing --capnp-public-address. These values are hard-coded
      (because they're just internal to the Docker container). *)
--- service/pipeline.ml
+++ service/pipeline.ml
@@ -110,31 +110,31 @@ module Matrix = struct
       let hash = head |> Github.Api.Commit.id |> Git.Commit_id.hash in
       let ci_url = url ~owner ~name ~hash in
       let hash = String.sub hash 0 7 in
-      match head |> Github.Api.Commit.id |> Git.Commit_id.gref |> parse_git_ref with 
+      match head |> Github.Api.Commit.id |> Git.Commit_id.gref |> parse_git_ref with
       | `PR p -> Fmt.pf f "PR <a href='https://github.com/%s/%s/pull/%s'>#%s</a>: job <a href='%a'><code><small>%s</small></code></a>" owner name p p Uri.pp ci_url hash
       | `Branch b -> Fmt.pf f "Branch <a href='https://github.com/%s/%s/tree/%s'>%s</a>: job <a href='%a'><code><small>%s</small></code></a>" owner name b b Uri.pp ci_url hash
 
     let pp_message_full ~head result f results =
-      let pp_message_job ~head f = Fmt.pf f "<li>%a</li>" (pp_message_job ~head) in 
-      Fmt.pf f "<h1>%s %a</h1> <ul>%a</ul>" 
-        (result_symbol result) 
-        pp_heading head 
+      let pp_message_job ~head f = Fmt.pf f "<li>%a</li>" (pp_message_job ~head) in
+      Fmt.pf f "<h1>%s %a</h1> <ul>%a</ul>"
+        (result_symbol result)
+        pp_heading head
         (Fmt.list (pp_message_job ~head)) (List.sort (fun (x, _) (y,_) -> String.compare x y) results)
 
   end
 
-  module Plain = struct 
+  module Plain = struct
 
     let pp_message_job ~head f (variant, (build, _)) =
       let { Github.Repo_id.owner; name } = Github.Api.Commit.repo_id head in
       let hash = Github.Api.Commit.hash head in
       let job_ci_url = url_variant ~owner ~name ~hash ~variant in
-      let result = 
+      let result =
         match build with
         | Ok `Checked | Ok `Built->
           "passed"
         | Error `Msg m when Astring.String.is_prefix ~affix:"[SKIP]" m ->
-          "skipped" 
+          "skipped"
         | Error `Msg m ->
           "failed: "^m
         | Error `Active _ ->
@@ -147,20 +147,20 @@ module Matrix = struct
       let hash = head |> Github.Api.Commit.id |> Git.Commit_id.hash in
       let ci_url = url ~owner ~name ~hash in
       let hash = String.sub hash 0 7 in
-      match head |> Github.Api.Commit.id |> Git.Commit_id.gref |> parse_git_ref with 
+      match head |> Github.Api.Commit.id |> Git.Commit_id.gref |> parse_git_ref with
       | `PR p -> Fmt.pf f "PR #%s (https://github.com/%s/%s/pull/%s): job %s (%a))" p owner name p hash Uri.pp ci_url
       | `Branch b -> Fmt.pf f "Branch %s (https://github.com/%s/%s/tree/%s): job %s (%a))" b owner name b hash Uri.pp ci_url
 
     let pp_message_full ~head result f results =
-      let pp_message_job ~head f = Fmt.pf f "- %a" (pp_message_job ~head) in 
-      Fmt.pf f "%s %a:\n%a" 
-        (result_symbol result) 
-        pp_heading head 
+      let pp_message_job ~head f = Fmt.pf f "- %a" (pp_message_job ~head) in
+      Fmt.pf f "%s %a:\n%a"
+        (result_symbol result)
+        pp_heading head
         (Fmt.list ~sep:Fmt.(const string "\n") (pp_message_job ~head)) (List.sort (fun (x, _) (y,_) -> String.compare x y) results)
 
   end
 
-  (* In order not to spam the channel on each CI update, we send a full message 
+  (* In order not to spam the channel on each CI update, we send a full message
      only when the final result is known. *)
   let message_of_state ~head result results =
     let+ head = head
@@ -169,38 +169,52 @@ module Matrix = struct
     in
     let result = Result.map (fun () -> `Checked) result in
     let open Matrix_common.Events.Event_content.Message in
-    let body, formatted_body = match result with 
-      | Ok _ | Error (`Msg _) -> 
+    let body, formatted_body = match result with
+      | Ok _ | Error (`Msg _) ->
         Fmt.to_to_string (Plain.pp_message_full ~head result) results,
         Fmt.to_to_string (Html.pp_message_full ~head result) results
-      | _ -> 
+      | _ ->
         Fmt.str "🟠 %a" (Plain.pp_heading) head,
         Fmt.str "<h2>🟠 %a</h2>" (Html.pp_heading) head
     in
     (Text (Text.make ~body ~format:"org.matrix.custom.html" ~formatted_body ()))
 
+(* power levels (https://matrix.org/docs/spec/client_server/latest#m-room-power-levels)
+   any user is 0
+   bot user is 100 (creator of the room)
+   any user in Conf.matrix_admins is 100
+
+   anyone can send any kind of message event
+   power level >50 to send state events (including to change power_levels)
+*)
+  let power_level_content_override =
+    let admins = Conf.matrix_admins |> List.map (fun admin -> (admin, 100)) in
+    let users = admins in
+    Current.return @@
+    Matrix_common.Events.Event_content.Power_levels.make ~users ()
+
   let get_room_for ~alias =
-    function 
-    | None -> None 
-    | Some context -> Some (Matrix_current.Room.make context ~alias ())
+    function
+    | None -> None
+    | Some context -> Some (Matrix_current.Room.make context ~alias ~power_level_content_override ())
 
   let get_org_room ~installation =
     let alias =
       let+ installation = installation in
       let account = Github.Installation.account installation in
       "ocaml-ci/" ^ account
-    in 
+    in
     get_room_for ~alias
 
-  let get_room ~repo = 
+  let get_room ~repo =
     let alias =
       let+ repo = repo in
       let id = Github.Api.Repo.id repo in
       (* we rely on the fact that allowed Github repository names is a subset of allowed Matrix channel names. *)
       "ocaml-ci/" ^ id.owner ^ "/" ^ id.name
-    in 
+    in
     get_room_for ~alias
-  
+
 end
 
 let set_active_installations installations =
@@ -377,9 +391,9 @@ let v ?ocluster ?matrix ~app ~solver () =
     |> github_status_of_state ~head summary
     |> Github.Api.CheckRun.set_status head "ocaml-ci"
   and set_matrix_status =
-    match matrix, matrix_room, matrix_org_room with 
+    match matrix, matrix_room, matrix_org_room with
     | None, None, None -> Current.return ()
-    | Some context, Some room, Some org_room -> 
+    | Some context, Some room, Some org_room ->
       let key =
         let+ head = head in
         Github.Api.Commit.id head
