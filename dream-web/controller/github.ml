99c1258260e414d0e375eac4738c9dfbf6299fe2
Navin Keswani <navin@novemberkilo.com>
2022-07-14 17:51:59 +1000

    Tidy routes.

--- dream-web/controller/github.ml
+++ dream-web/controller/github.ml
@@ -15,14 +15,14 @@ let list_orgs ci =
   Client.CI.orgs ci >>!= fun orgs ->
   Dream.respond @@ View.Github.list_orgs ~orgs
 
-let list_repos org ci =
+let list_repos ~org ci =
   let list_repos' org o =
     Client.Org.repos o >>!= fun repos ->
     Dream.respond @@ View.Github.list_repos ~org ~repos
   in
   Capability.with_ref (Client.CI.org ci org) @@ list_repos' org
 
-let list_refs org name ci =
+let list_refs ~org ~name ci =
   let list_refs' org name r =
     Client.Repo.refs r >>!= fun refs ->
     Dream.respond @@ View.Github.list_refs ~org ~name ~refs
@@ -30,20 +30,3 @@ let list_refs org name ci =
   Capability.with_ref (Client.CI.org ci org) @@ fun org' ->
   Capability.with_ref (Client.Org.repo org' name) @@ fun repo ->
   list_refs' org name repo
-
-let get_handler ~path ci =
-  match path with
-  | [] -> list_orgs ci
-  | [ org ] -> list_repos org ci
-  | [ org; name ] -> list_refs org name ci
-  | [ org; name; path ] ->
-      Dream.log "Not implemented yet %s %s %s" org name path;
-      not_found
-  | _ ->
-      Dream.log "Not supported";
-      not_found
-
-let post_handler ~path _ =
-  Dream.log "POST handlers not yet implemented. Path: %s"
-    (String.concat "/" path);
-  not_found
--- dream-web/main.ml
+++ dream-web/main.ml
@@ -1,52 +1,49 @@
-open Astring
-
 let main port backend_cap =
-  let github_path request =
-    let path_components =
-      Dream.target request |> Uri.pct_decode
-    in
-    match String.cuts ~sep:"/" ~empty:false path_components with
-    | "github" :: path -> path
-    | _ -> []
-  in  
-  Lwt_main.run begin
-      let open Lwt.Infix in
-
-      Backend.Make.ci backend_cap >>= fun ci ->
-      Dream.serve ~port
-      @@ Dream.logger
-      @@ Dream.origin_referrer_check
-      @@ Dream.router [
-             Dream.get "/css/**" @@ Dream.static "dream-web/static/css";
-             Dream.get "/" (fun _ -> Dream.html @@ Controller.Index.render);
-             Dream.get "/github" (fun _request -> Controller.Github.get_handler ~path:[] ci);
-             Dream.scope"/github" [Dream.origin_referrer_check] [
-               Dream.get "**" (fun request ->
-                     Controller.Github.get_handler ~path:(github_path request) ci)
-               ];
-               Dream.post "**" (fun request ->
-                     Controller.Github.post_handler ~path:(github_path request) ci)
-           ]
-    end
-
+  Lwt_main.run
+    (let open Lwt.Infix in
+    Backend.Make.ci backend_cap >>= fun ci ->
+    Dream.serve ~port @@ Dream.logger @@ Dream.origin_referrer_check
+    @@ Dream.router
+         [
+           Dream.get "/css/**" @@ Dream.static "dream-web/static/css";
+           Dream.get "/" (fun _ -> Dream.html @@ Controller.Index.render);
+           Dream.get "/github" (fun _request -> Controller.Github.list_orgs ci);
+           Dream.get "/github/:org" (fun request ->
+               Controller.Github.list_repos ~org:(Dream.param request "org") ci);
+           Dream.get "github/:org/:repo" (fun request ->
+               Controller.Github.list_refs
+                 ~org:(Dream.param request "org")
+                 ~name:(Dream.param request "repo")
+                 ci);
+           Dream.get "github/:org/:repo/commit/:hash" (fun request ->
+               Dream.log "Not implemented yet %s %s %s"
+                 (Dream.param request "org")
+                 (Dream.param request "repo")
+                 (Dream.param request "hash");
+               Dream.respond ~status:`Not_Found View.Client_error.not_found);
+           Dream.get "github/:org/:repo/commit/:hash/variant/:variant"
+             (fun request ->
+               Dream.log "Not implemented yet %s %s %s %s"
+                 (Dream.param request "org")
+                 (Dream.param request "repo")
+                 (Dream.param request "hash")
+                 (Dream.param request "variant");
+               Dream.respond ~status:`Not_Found View.Client_error.not_found);
+         ])
 
 open Cmdliner
 
 let port =
-  Arg.value @@
-    Arg.opt Arg.int 8090 @@
-      Arg.info
-        ~doc:"The port on which to listen for incoming HTTP connections."
-        ~docv:"PORT"
-        ["port"]
+  Arg.value @@ Arg.opt Arg.int 8090
+  @@ Arg.info ~doc:"The port on which to listen for incoming HTTP connections."
+       ~docv:"PORT" [ "port" ]
 
 let backend_cap =
-  Arg.required @@
-    Arg.opt (Arg.some Capnp_rpc_unix.sturdy_uri) None @@
-      Arg.info
-        ~doc:"The capability file giving access to the CI backend service."
-        ~docv:"CAP"
-        ["backend"]
+  Arg.required
+  @@ Arg.opt (Arg.some Capnp_rpc_unix.sturdy_uri) None
+  @@ Arg.info
+       ~doc:"The capability file giving access to the CI backend service."
+       ~docv:"CAP" [ "backend" ]
 
 let cmd =
   let doc = "A web front-end for OCaml-CI" in
