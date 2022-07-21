1221214dbd6e36c38cab327ad56063a15b439555
Navin Keswani <navin@novemberkilo.com>
2022-07-21 19:03:38 +1000

    Clean out unused eml template files.

--- dream-web/view/client_error.eml.ml
+++
@@ -1,5 +0,0 @@
-let not_found_contents =
-  <p>Not Found</p>
-
-let not_found =
-  Template.instance not_found_contents
--- dream-web/view/index.eml.ml
+++
@@ -1,9 +0,0 @@
-let contents =
-  <p>Welcome to OCaml-CI!</p>
-                 <p>See <a href='https://github.com/apps/ocaml-ci'>The Ocaml-CI GitHub App</a> for details</p>
-                 <ul>
-                 <li><a href='/github'>Registered GitHub organisations</a></li>
-  </ul>
-
-let render =
-  Template.instance contents
