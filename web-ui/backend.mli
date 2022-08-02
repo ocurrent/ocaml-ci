0a48fd698bdfb8fba09eaceb17fc327c318bbf14
Navin Keswani <navin@novemberkilo.com>
2022-08-02 12:17:38 +1000

    Remove web-ui.

--- web-ui/backend.mli
+++
@@ -1,7 +0,0 @@
-open Capnp_rpc_lwt
-
-type t
-
-val make : Ocaml_ci_api.Raw.Client.CI.t Sturdy_ref.t -> t
-
-val ci : t -> Ocaml_ci_api.Client.CI.t Lwt.t
--- web-ui/github.mli
+++
@@ -1,3 +0,0 @@
-val handle :
-  backend:Backend.t -> meth:Cohttp.Code.meth -> string list ->
-  Cohttp_lwt_unix.Server.response_action Lwt.t
