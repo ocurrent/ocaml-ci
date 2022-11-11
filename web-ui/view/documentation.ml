e852cf4215b219604b61c755411b23bcbd99bc96
Navin Keswani <navin@novemberkilo.com>
2022-11-11 16:59:45 +1100

    Improve getting-started documentation with images.

--- web-ui/view/documentation.ml
+++ web-ui/view/documentation.ml
@@ -34,19 +34,32 @@ let getting_started =
                   li
                     [
                       txt "Add the app to your account, selecting ";
-                      strong [ txt "Install" ];
+                      strong [ txt "Configure" ];
                       txt " at ";
                       a
                         ~a:[ a_href "https://github.com/apps/ocaml-ci" ]
                         [ txt "https://github.com/apps/ocaml-ci" ];
+                      img ~src:"/images/github-apps-1.png"
+                        ~alt:"Click Configure" ();
                     ];
                   li
                     [
                       txt
-                        "Select only the repositories you want tested - if you \
+                        "Select the GitHub account or organisation and click \
+                         configure.";
+                      img ~src:"/images/github-apps-2.png"
+                        ~alt:"Click Configure" ();
+                    ];
+                  li
+                    [
+                      txt
+                        "Select only the repositories you want tested \
+                         (starting with no more than three please). If you \
                          select ";
                       strong [ txt "All Repositories" ];
                       txt " we won't build anything!";
+                      img ~src:"/images/github-apps-3.png"
+                        ~alt:"Click Configure" ();
                     ];
                 ];
               p
@@ -59,7 +72,8 @@ let getting_started =
                     [ txt "https://github.com/ocurrent" ];
                   txt
                     ". You will need to be approved before it will start \
-                     building anything. e.g. ";
+                     building anything. This is done by adding yourself to a \
+                     list by submitting a PR - e.g. ";
                   a
                     ~a:
                       [ a_href "https://github.com/ocurrent/ocaml-ci/pull/346" ]
