9d7abf6a27627fc9b5d80473af20b84e56e72cd3
Navin Keswani <navin@novemberkilo.com>
2022-11-29 08:14:18 +1100

    Remove backticks from tailwind's code blocks.
    https://futurestud.io/tutorials/tailwind-css-remove-backticks-around-inline-code

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,30 @@ module.exports = {
           50: "#FFFAEB",
           500: "#F79009",
         }
+      },
+      typography (theme) {
+        return {
+          DEFAULT: {
+            css: {
+              'code::before': {
+                content: 'none', // don’t generate the pseudo-element
+//                content: '""', // this is an alternative: generate pseudo element using an empty string
+              },
+              'code::after': {
+                content: 'none'
+              },
+              code: {
+                color: theme('colors.slate.500'),
+                backgroundColor: theme('colors.stone.100'),
+                borderRadius: theme('borderRadius.DEFAULT'),
+                paddingLeft: theme('spacing[1.5]'),
+                paddingRight: theme('spacing[1.5]'),
+                paddingTop: theme('spacing.1'),
+                paddingBottom: theme('spacing.1'),
+              },
+            }
+          }
+        }
       }
     },
   },
