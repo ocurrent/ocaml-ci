6e186d2f196e8fe106940ed43a5df4a6a59f24c9
Navin Keswani <navin@novemberkilo.com>
2022-11-29 09:53:11 +1100

    Setup to read profile-pictures from disk.

--- web-ui/view/organisation.ml
+++ web-ui/view/organisation.ml
@@ -5,24 +5,24 @@ module Make (M : M_Git_forge) = struct
     (* FIXME [benmandrew]: How can we get the GitLab profile pictures? *)
     let local_image =
       match M.prefix with
-      | "github" -> Printf.sprintf "/images/profile-pictures/github/%s.png" org
+      | "github" -> Printf.sprintf "/profile-pictures/github/%s.png" org
       | "gitlab" -> "/images/gitlab-logo-500.png"
       | _ -> ""
     in
     let fallback_image = Printf.sprintf "/images/%s-logo-500.png" M.prefix in
     let local_image_exists =
       try
-        let _ = Unix.stat (Printf.sprintf "./web-ui/static%s" local_image) in
+        let _ = Unix.stat local_image in
         true
       with _ -> false
     in
     let url = if local_image_exists then local_image else fallback_image in
-  Tyxml.Html.(
-    img
-      ~a:[ a_class [ "w-20 h-20 rounded-full" ] ]
-      ~src:url
-      ~alt:(Printf.sprintf "%s profile picture" org)
-      ())
+    Tyxml.Html.(
+      img
+        ~a:[ a_class [ "w-20 h-20 rounded-full" ] ]
+        ~src:url
+        ~alt:(Printf.sprintf "%s profile picture" org)
+        ())
 
   let logo =
     match M.prefix with
