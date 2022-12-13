e29eff2aaefbf065ce98dfb477c7f63bf1d0fb25
benmandrew <benmandrew@gmail.com>
2022-12-13 16:36:56 +0100

    In progress

--- web-ui/static/js/repo-page.js
+++ web-ui/static/js/repo-page.js
@@ -74,7 +74,85 @@ const chartOptions = {
     display: false
   },
   tooltips: {
-    enabled: false
+    // Hide the on-canvas tooltip
+    enabled: false,
+    custom: function(tooltipModel) {
+      // Tooltip Element
+      var tooltipEl = document.getElementById('chartjs-tooltip');
+
+      // Create element on first render
+      if (!tooltipEl) {
+          tooltipEl = document.createElement('div');
+          tooltipEl.id = 'chartjs-tooltip';
+          tooltipEl.innerHTML = "";
+          // tooltipEl.innerHTML = '<table></table>';
+          document.body.appendChild(tooltipEl);
+      }
+
+      // Hide if no tooltip
+      if (tooltipModel.opacity === 0) {
+          tooltipEl.style.opacity = 0;
+          return;
+      }
+
+      // Set caret Position
+      tooltipEl.classList.remove('above', 'below', 'no-transform');
+      if (tooltipModel.yAlign) {
+          tooltipEl.classList.add(tooltipModel.yAlign);
+      } else {
+          tooltipEl.classList.add('no-transform');
+      }
+
+      function getBody(bodyItem) {
+          return bodyItem.lines;
+      }
+
+      // Set Text
+      if (tooltipModel.body) {
+          // var titleLines = tooltipModel.title || [];
+          var bodyLines = tooltipModel.body.map(getBody);
+
+
+          // console.log(titleLines);
+          // console.log(bodyLines);
+
+          // var innerHtml = '<thead>';
+
+          // titleLines.forEach(function(title) {
+          //     innerHtml += '<tr><th>' + title + '</th></tr>';
+          // });
+          // innerHtml += '</thead><tbody>';
+
+          // bodyLines.forEach(function(body, i) {
+          //     var colors = tooltipModel.labelColors[i];
+          //     var style = 'background:' + colors.backgroundColor;
+          //     style += '; border-color:' + colors.borderColor;
+          //     style += '; border-width: 2px';
+          //     var span = '<span style="' + style + '"></span>';
+          //     innerHtml += '<tr><td>' + span + body + '</td></tr>';
+          // });
+          // innerHtml += '</tbody>';
+
+          innerHtml = "<p>" + bodyLines[0][0] + "</p>";
+
+          // var tableRoot = tooltipEl.querySelector('table');
+          tooltipEl.innerHTML = innerHtml;
+      }
+
+      // `this` will be the overall tooltip
+      var position = this._chart.canvas.getBoundingClientRect();
+
+      // Display, position, and set styles for font
+      tooltipEl.style.opacity = 1;
+      tooltipEl.style.position = 'absolute';
+      tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+      tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+      tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+      tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+      tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+      tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+      tooltipEl.style.pointerEvents = 'none';
+  },
   },
   elements: {
     point: {
@@ -96,8 +174,8 @@ const chartOptions = {
         display: false,
         suggestedMin: 0,
         suggestedMax: 10
-      }
-    }]
+      },
+    }],
   },
 };
 
@@ -164,7 +242,4 @@ window.onload = function() {
   body = table_root.lastChild;
 
   charts_init();
-  // Hack to make the bar charts consistently sized, without this on
-  // page load they are of different sizes until the window is resized.
-  window.dispatchEvent(new Event('resize'));
 }
