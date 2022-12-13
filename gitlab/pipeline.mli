5f02b304f79e56722b02f68b21a2712c6cfd07d9
Alpha DIALLO <moyodiallo@gmail.com>
2022-12-13 18:52:37 +0100

    Some fix after rebase

--- gitlab/pipeline.mli
+++ gitlab/pipeline.mli
@@ -1,12 +1,15 @@
 val local_test :
-  solver:Ocaml_ci_api.Solver.t -> Current_git.Local.t -> unit -> unit Current.t
+  solver:Ocaml_ci.Backend_solver.t ->
+  Current_git.Local.t ->
+  unit ->
+  unit Current.t
 (** [local_test ~solver repo] is a pipeline that tests local repository [repo]
     as the CI would. *)
 
 val v :
   ?ocluster:Cluster_api.Raw.Client.Submission.t Capnp_rpc_lwt.Sturdy_ref.t ->
   app:Current_gitlab.Api.t ->
-  solver:Ocaml_ci_api.Solver.t ->
+  solver:Ocaml_ci.Backend_solver.t ->
   migrations:string option ->
   unit ->
   unit Current.t
