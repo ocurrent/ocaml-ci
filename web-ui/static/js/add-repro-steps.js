8df3076a9036ef64ac0ec0eee89282549dab7202
Navin <navin@tarides.com>
2022-12-14 04:07:42 +1100

    Adding build-repro section to the step page. (#681)
    * Adding build-repro section to the step page.
    
    * Hide repro for old logs that don't have the required tags
    
    Co-authored-by: benmandrew <benmandrew@gmail.com>

---
+++ web-ui/static/js/add-repro-steps.js
@@ -0,0 +1,23 @@
+function extractStepsToReproduce() {
+  const tableRows = document.getElementById("steps-table");
+  const start = document.getElementsByClassName("repro-block-start");
+  const finish = document.getElementsByClassName("repro-block-end");
+  if ((start.length === 0) || (finish.length === 0)) {
+    document.getElementById("build-repro-container").style.display = 'none';
+    return null
+  };
+  const startIndex = parseInt(start[0].parentElement.id.split('L')[1], 10);
+  const finishIndex = parseInt(finish[0].parentElement.id.split('L')[1], 10);
+
+  const br = document.createElement("br");
+  const reproDiv = document.getElementById("build-repro");
+
+  for (let i = (startIndex); i < (finishIndex - 1); i++) {
+    // Remove line-number, whitespace and possible newlines from the row
+    const newContent = document.createTextNode(tableRows.rows[i].innerText.replace(/\d+\s*\n?/, ''));
+    reproDiv.appendChild(newContent);
+    reproDiv.appendChild(br.cloneNode());
+  }
+}
+
+extractStepsToReproduce();
