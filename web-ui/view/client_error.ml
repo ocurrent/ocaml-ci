284ea8bcc7362bcaec2537df158ecd60a7776c5d
Etienne MARAIS <etienne@maiste.fr>
2022-12-13 17:59:23 +0100

    Error page dark mode

--- web-ui/view/client_error.ml
+++ web-ui/view/client_error.ml
@@ -25,8 +25,8 @@ let header =
       [
         a_class
           [
-            "h-20 flex items-center border-b border-gray-200 bg-white sticky \
-             top-0 z-50";
+            "h-20 flex items-center border-b border-gray-200 \
+             dark:border-gray-700 bg-white dark:bg-gray-850 sticky top-0 z-50";
           ];
       ]
     [
