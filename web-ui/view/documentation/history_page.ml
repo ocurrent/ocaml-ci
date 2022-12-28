173a2c9ce8381175c5568374984b77419c5df7ed
Christine Rose <christinerose@users.noreply.github.com>
2022-12-28 10:43:30 -0800

    Minor grammar

--- web-ui/view/documentation/history_page.ml
+++ web-ui/view/documentation/history_page.ml
@@ -7,8 +7,8 @@ let show =
       [
         txt
           "The history page shows the build history of a ref. It shows every \
-           commit of that branch that OCaml-CI built. Each row that appears on \
-           this page corresponds to the latest build of a commit and clicking \
+           commit of that branch built by OCaml-CI. Each row that appears on \
+           this page corresponds to the latest build of a commit, and clicking \
            on a row takes you to the build page of that commit. Each row \
            identifies the commit, its build status, the time at which the \
            build started, and the running time of the build.";
