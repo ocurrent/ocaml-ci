7680d01a2470a35b17307473d90407bb25b4506d
Tim McGilchrist <timmcgil@gmail.com>
2023-01-04 15:52:17 +1100

    Tidy up tests and remove un-used solver code.

--- test/service/test.ml
+++ test/service/test.ml
@@ -1,7 +1,1 @@
-let () =
-  Unix.putenv "HOME" "/idontexist";
-  (* Ignore user's git configuration *)
-  Unix.putenv "GIT_AUTHOR_NAME" "test";
-  Unix.putenv "GIT_COMMITTER_NAME" "test";
-  Unix.putenv "EMAIL" "test@example.com";
-  Alcotest.run "ocaml-ci" [ ("obuilder_specs", Test_spec.tests) ]
+let () = Alcotest.run "ocaml-ci" [ ("obuilder_specs", Test_spec.tests) ]
