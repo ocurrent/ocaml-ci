e10e3bf31a1473daebb80a90f00656d563a2d0f6
Etienne MARAIS <etienne@maiste.fr>
2023-01-06 17:22:13 +0100

    Integrate #703 solution in ocaml-ci-service

--- service/pipeline.ml
+++ service/pipeline.ml
@@ -292,9 +292,21 @@ let v ?ocluster ~app ~solver ~migrations () =
            let analysis =
              Analyse.examine ~solver ~platforms ~opam_repository_commit src
            in
-           let on_cancel =
-             match ocluster with None -> None | Some _ -> Some ignore
+           let* on_cancel =
+             match ocluster with
+             | None -> Current.return None
+             | Some _ ->
+                 let+ commit = head in
+                 let gref = ref_from_commit commit in
+                 let repo = Current_github.Api.Commit.repo_id commit in
+                 let repo =
+                   { Ocaml_ci.Repo_id.owner = repo.owner; name = repo.name }
+                 in
+                 let hash = Current_github.Api.Commit.hash commit in
+                 Some
+                   (fun _ -> Index.record_summary_on_cancel ~repo ~gref ~hash)
            in
+
            let builds =
              let repo = Current.map Github.Api.Repo.id repo in
              build_with_docker ?ocluster ?on_cancel ~repo ~analysis ~platforms
