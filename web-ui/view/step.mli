cf3930c93809bfa9afde6e212fe1e6f1543d98ce
Navin <navin@tarides.com>
2023-04-04 11:21:08 +1000

    Add step-logs.js to crunch. (#799)
    * Revert "Revert "Logs over websockets (#794)""
    
    This reverts commit 06bb80a7120918a9372f9722ac12a2df4e9727c5.
    
    * Update crunch rules to bring in step-logs.js
    
    * Updates shas in Dockerfile.web to latest and drop dream pins.
    
    * Cleanup following review comments.
    
    * Update dune-project
    
    Co-authored-by: Tim McGilchrist <timmcgil@gmail.com>
    
    ---------
    
    Co-authored-by: Tim McGilchrist <timmcgil@gmail.com>

--- web-ui/view/step.mli
+++ web-ui/view/step.mli
@@ -32,5 +32,5 @@ module Make : functor (_ : Git_forge_intf.Forge) -> sig
     can_cancel:bool ->
     ?flash_messages:(string * string) list ->
     string * int64 ->
-    Dream.response Lwt.t
+    string
 end
