bfe05e3742921f450609b1ec817b6980b31ae4a5
Antonin Décimo <antonin@tarides.com>
2023-06-01 10:11:55 +0200

    Temporarily disable OpenSUSE Tumbleweed
    opam currently doesn't recognize Tumbleweed properly and fails depext
    installation.

--- service/conf.ml
+++ service/conf.ml
@@ -178,6 +178,7 @@ let platforms ~ci_profile ~include_macos opam_version =
   | `Production ->
       let distros =
         DD.active_tier1_distros `X86_64 @ DD.active_tier2_distros `X86_64
+        |> List.filter (( <> ) (`OpenSUSE `Tumbleweed :> DD.t))
         |> List.concat_map make_distro
       in
       let distros =
