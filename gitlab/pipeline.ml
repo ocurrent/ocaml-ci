9a39bac30436f1626c8eef4332820ee6891903df
Tim McGilchrist <timmcgil@gmail.com>
2023-08-21 10:42:25 +1000

    Remove maiste/ocaml-ci-experiment.
    Project no longer available on gitlab.com

--- gitlab/pipeline.ml
+++ gitlab/pipeline.ml
@@ -96,11 +96,6 @@ let gitlab_repos : Gitlab.Repo_id.t list =
       Gitlab.Repo_id.name = "tezt";
       project_id = 41852995;
     };
-    {
-      Gitlab.Repo_id.owner = "maiste";
-      Gitlab.Repo_id.name = "ocaml-ci-experiment";
-      project_id = 40839423;
-    };
   ]
 
 (* Fake Installation module, we don't have this in GitLab. *)
