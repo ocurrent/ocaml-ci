267ec98f89acaed0fae9d67e160f159a31910b47
Mark Elvers <mtelvers@users.noreply.github.com>
2024-08-01 17:29:41 +0100

    fix typo in string
    Co-authored-by: Antonin Décimo <antonin.decimo@gmail.com>

--- lib/opam_version.ml
+++ lib/opam_version.ml
@@ -5,7 +5,7 @@ let to_string = function `V2_0 -> "2.0" | `V2_1 -> "2.1" | `V2_2 -> "2.2"
 let to_string_with_patch = function
   | `V2_0 -> "2.0.10"
   | `V2_1 -> "2.1.6"
-  | `V2_2 -> "2.0.0"
+  | `V2_2 -> "2.2.0"
 
 let pp = Fmt.of_to_string to_string
 let default = `V2_0
